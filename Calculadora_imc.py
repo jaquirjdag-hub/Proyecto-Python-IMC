a26844c921194fb48ed6094dc57fff29902ec365
Jacqueline <jaqui.rjdag@gmail.com>
2026-09-07 14:35:37 -0600

    agrego archivos de Modulo_1

---
+++ Calculadora_imc.py
@@ -0,0 +1,24 @@
+# Nombre: se espera un dato de tipo (string)
+nombre = "Rosa Jacqueline"
+
+# Apellido paterno: se espera un dato de tipo (string)
+apellido_paterno = "Chino"
+
+#Apellido materno: se espera un dato de tipo (string)
+apellido_materno = "Diaz"
+
+#edad: se espera un dato de tipo (int)
+edad = 27
+
+#Pesoen kilogramos: se espera un tipo de dato (float)
+peso = 46.50
+
+# Estatura en metros: se espera un tipo de dato (float)
+estatura = 1.53
+
+# Mensaje de Bienvenida
+print("¡Bienvenidos al programa calculadora de IMC!")
+
+
+
+
---
+++ Variables.py
@@ -0,0 +1,16 @@
+
+Chocolate_caliente = 50
+
+#variable float (decimal)
+Pi = 3.141516
+
+#variable boolean 
+is_active = True
+
+#variable string/char (texto)
+Saludo = "Hola, mundo!"
+
+print("Tengo un Chocolate caliente que vale:", Chocolate_caliente)
+#f-atring
+
+print("este es mi primer codigo en python")
