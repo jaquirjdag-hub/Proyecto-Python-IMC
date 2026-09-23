a07c0bb7d2534ce6623647c6644471b6cbd53a31
ROsa Jacqueline Chino Díaz <jaqui.rjdag@gmail.com>
2026-09-23 14:39:05 -0600

    semana_2

--- Modulo_1/Variables.py
+++ Modulo_1/Variables.py
@@ -1,39 +1,45 @@
-#variable int (entero)
-Chocolate_caliente = 50
+#Variable int (entero)
+chocolate_caliente = 50
 
-#variable float (decimal)
-Pi = 3.141516
+#Variable float (decimal)
+PI = 3.141516
 
-#variable boolean 
-
+#Variable boolean 
 is_active = True
 
-#variable string/char (texto)
-Saludo = "Hola, mundo!"
+#Variable string/char (texto)
+Saludo = 'Hola, mundo!'
+
+#print("tengo un chocolate caliente que vale:", chocolate_caliente, "pesos mexicamos.")
+
 
 #f-String
-print(f'variables: {Chocolate_caliente}, {Pi}, {is_active}, {Saludo}')
+#print(f'Tengo un chocolate caliente que vale: {chocolate_caliente + 1} pesos mexicanos.')
+
+#print(f'Variables simples: {chocolate_caliente}, {PI}, {is_active}, {Saludo}')
 
 #---------
 #Variables list
-#[lindexo,  lindex1, ......]
-fruits = ["apple", 50, "cherry", [1,2,3], True]
-#fruits[0]---- 'apple'
+#[lindexo,  lindex1, index2, ...]
+fruits = ['apple', 50, 'cherry', [1,2,3], True]
+#fruits[0]-- 'apple'
 
 #Variable Tuple
-#(VALUE1, VALUE2, .....)
+#(value1, value2, ...)
 coordinates = (10.0, 20.0)
 #coordinates[0]---- 10.0
 #coordinates[1]---- 20.0
 
 #Variable Dictionary
-#{ Key : value}
-person ={
-    'name' : 'kevin',
-    'age' : 30,
-    'city' : 'Madrid',
-    'hobies' : ['football', 'read','trips'],
-    'is_active' : True
-}
- 
-print(f'Variables complejas: {fruits}, {coordinates}, {person}')
+#{key : value}
+#
+#-------------------
+#Ejemplo de conversión de tipos
+numero_str = "100"
+
+#a + b a y b deben ser del mismo tipo (decimal, enteros)
+#No podemos sumar/concatenar un string con un entero directamente
+print(f'{numero_str}50') #output "10050"
+
+numero_int  = int(numero_str) + 50 #conversión de string a entero y sumar 50
+print(numero_int)
